bdaeb44cd09cd66cb7fe7c172d2d0f853f4cbea2
carlosarmenta75 <145488513+carlosarmenta75@users.noreply.github.com>
2026-02-20 00:56:46 -0800

    Updated side-nav.jsx

--- src/components/dashboard/layout/vertical/side-nav.jsx
+++ src/components/dashboard/layout/vertical/side-nav.jsx
@@ -51,7 +51,7 @@ export function SideNav({ color = "evident", items = [] }) {
 			<Stack spacing={2} sx={{ p: 2 }}>
 				<div>
 					<Box component={RouterLink} href={paths.home} sx={{ display: "inline-flex" }}>
-						<Logo color={logoColor} height={40} />
+						<Logo color={logoColor} height={80} />
 					</Box>
 				</div>
 				<WorkspacesSwitch />
